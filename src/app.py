bf558a0f04c88db892b379bc10d38bf2dad47f0b
jronald88 <156713858+jronald88@users.noreply.github.com>
2024-07-22 19:16:22 +0000

    latest

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
+
 import os
 os.system("pip install --upgrade pandas")
-from sqlalchemy import create_engine
+os.system("python -m pip install --upgrade 'sqlalchemy<2.0'")
+from sqlalchemy import create_engine, inspect, text
 import pandas as pd
 import numpy as np
 from dotenv import load_dotenv
@@ -12,16 +14,19 @@ load_dotenv()
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
 connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 engine = create_engine(connection_string).execution_options(autocommit=True)
-engine.connect()
+conn = engine.connect()
+
+
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+engine.execute("""DROP TABLE publishers, books, book_authors, authors CASCADE;""")
 engine.execute("""
 CREATE TABLE publishers(
     publisher_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY(publisher_id)
 );
-
+      
 CREATE TABLE authors(
     author_id INT NOT NULL,
     first_name VARCHAR(100) NOT NULL,
@@ -48,29 +53,24 @@ CREATE TABLE book_authors (
     PRIMARY KEY(book_id, author_id),
     CONSTRAINT fk_book FOREIGN KEY(book_id) REFERENCES books(book_id) ON DELETE CASCADE,
     CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES authors(author_id) ON DELETE CASCADE
-);
+);     
+
 """)
-# Check for existing DB
+
 inspector = inspect(engine)
-for table_name in ['publishers', 'authors', 'books', 'book_authors']:
-    if not inspector.has_table(table_name):
-        Base.metadata.create_all(engine)
-        print(f"Created table: {table_name}")
-    else:
-        print(f"Table '{table_name}' already exists. Skipping creation.")
+tables = inspector.get_table_names()  # Get all table names
+print(tables)
 
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
-
-engine.execute("""-- publishers
-INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
+engine.execute("""INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
 INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart');
 INSERT INTO publishers(publisher_id, name) VALUES (3, 'A K PETERS');
 INSERT INTO publishers(publisher_id, name) VALUES (4, 'Academic Press');
 INSERT INTO publishers(publisher_id, name) VALUES (5, 'Addison Wesley');
 INSERT INTO publishers(publisher_id, name) VALUES (6, 'Albert&Sweigart');
 INSERT INTO publishers(publisher_id, name) VALUES (7, 'Alfred A. Knopf');
+               
 
--- authors 
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (1, 'Merritt', null, 'Eric');
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (2, 'Linda', null, 'Mui');
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (3, 'Alecos', null, 'Papadatos');
@@ -80,7 +80,6 @@ INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (6, '
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (7, 'Yuval', 'Noah', 'Harari');
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (8, 'Paul', null, 'Albitz');
 
--- books
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (1, 'Lean Software Development: An Agile Toolkit', 240, 4.17, '9780320000000', '2003-05-18', 5);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (2, 'Facing the Intelligence Explosion', 91, 3.87, null, '2013-02-01', 7);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (3, 'Scala in Action', 419, 3.74, '9781940000000', '2013-04-10', 1);
@@ -92,7 +91,6 @@ INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, pu
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (9, 'The Apollo Guidance Computer: Architecture And Operation (Springer Praxis Books / Space Exploration)', 439, 4.29, '9781440000000', '2010-07-01', 6);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (10, 'Minds and Computers: An Introduction to the Philosophy of Artificial Intelligence', 222, 3.54, '9780750000000', '2007-02-13', 7);
 
--- book authors
 INSERT INTO book_authors (book_id, author_id) VALUES (1, 1);
 INSERT INTO book_authors (book_id, author_id) VALUES (2, 8);
 INSERT INTO book_authors (book_id, author_id) VALUES (3, 7);
@@ -107,6 +105,12 @@ INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);
 
 
 
+
 # 4) Use pandas to print one of the tables as dataframes using read_sql function
-result_dataFrame = pd.read_sql("Select * from publishers;", engine)
-print(result_dataFrame)
+with engine.connect() as conn:
+    df = pd.read_sql(
+        sql="SELECT * FROM authors;",
+        con=conn.connection
+    )
+
+print(df.head())
